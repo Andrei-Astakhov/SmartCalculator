855404b8005749dfa19e86cb03e3b8dcb822b587
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-02-08 08:02:03 +0300

    Stage 2/8: 2+2+

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>) {
+package calculator
 
+fun main() {
+    while (true) {
+        val inputString = readLine()!!
+        when {
+            inputString.isBlank() -> continue
+            inputString == "/exit" -> break
+            !inputString.contains(" ") -> {
+                println(inputString)
+                continue
+            }
+            else -> {
+                val (a, b) = inputString.split(" ")
+                val sum = a.toInt() + b.toInt()
+                println(sum)
+            }
+        }
+    }
+    println("Bye!")
 }
