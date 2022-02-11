78e1a34f2d7d3a2416ee044d4d24e0b381e78226
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-02-11 15:00:21 +0300

    Stage 6/8: Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,82 @@
 package calculator
 
 class Calculator {
+    val commandRegex = Regex("""/[a-z]+""")
+    val expressionRegex = Regex("""[-+]?(\d+|[a-zA-Z]+)(\s*[-+]*\s*-?(\d+|[a-zA-Z]+))*""")
+    val variableRegex = Regex("""\s*\w+(\s*=\s*\w+\s*)*""")
+    val globalVariables = mutableMapOf<String, Int>()
+    val errors = mapOf(
+        "InvalidAssignment" to "Invalid assignment",
+        "InvalidExpression" to "Invalid expression",
+        "InvalidIdentifier" to "Invalid identifier",
+        "UnknownCommand" to "Unknown command",
+        "UnknownVariable" to "Unknown variable"
+    )
+
     fun run() {
         while (true) {
             val inputString = readLine()!!
-            val expressionRegex = Regex("""[-+]?\d+( [-+]* -?\d+)*""")
-
-            val commandRegex = Regex("""/[a-zA-Z]+""")
-            when {
-                inputString.isBlank() -> continue
-                inputString.matches(expressionRegex) -> calculate(inputString)
-                inputString.matches(commandRegex) -> {
-                    when (inputString) {
-                        "/exit" -> break
-                        "/help" -> showHelp()
-                        else -> println("Unknown command")
+            try {
+                when {
+                    inputString.isBlank() -> continue
+                    inputString.matches(variableRegex)  -> processVariable(inputString)
+                    inputString.matches(expressionRegex) -> calculate(inputString)
+                    inputString.matches(commandRegex) -> {
+                        when (inputString) {
+                            "/exit" -> break
+                            "/help" -> showHelp()
+                            else -> printError("UnknownCommand")
+                        }
                     }
+                    else -> printError("InvalidExpression")
                 }
-                else -> println("Invalid expression")
+            } catch(e: Exception) {
+                continue
             }
         }
         println("Bye!")
+    }
+
+    fun processVariable(expression: String) {
+        val variables = expression.split("""\s*=\s*""".toRegex())
+        when (variables.size) {
+            1 -> printVariable(variables.first())
+            2 -> setVariable(variables)
+            else -> printError("InvalidAssignment")
+        }
+    }
+
+    fun printError(errorCode: String) {
+        if(errors.containsKey(errorCode)) println(errors.getValue(errorCode))
+        else println("UNKNOWN ERROR")
+    }
+
+    fun printVariable(variable: String) {
+        if (!variable.matches("""[a-zA-Z]+""".toRegex())) {
+            printError("InvalidIdentifier")
+        } else if (globalVariables.containsKey(variable)) {
+            println(globalVariables[variable])
+        } else {
+            printError("UnknownVariable")
+        }
+    }
+
+    fun setVariable(variables: List<String>) {
+        val leftValue = variables.first().trim()
+        val rightValue = variables.last().trim()
+
+        when {
+            !leftValue.matches("""[a-zA-Z]+""".toRegex()) -> printError("InvalidIdentifier")
+            rightValue.matches("""\d+""".toRegex()) -> globalVariables[leftValue] = rightValue.toInt()
+            rightValue.matches("""[a-zA-Z]+""".toRegex()) -> {
+                if (globalVariables.containsKey(rightValue)) {
+                    globalVariables[leftValue] = globalVariables[rightValue]!!.toInt()
+                } else {
+                    printError("UnknownVariable")
+                }
+            }
+            else -> printError("InvalidAssignment")
+        }
     }
 
     fun showHelp() {
@@ -33,17 +89,32 @@ class Calculator {
     fun calculate(expression: String) {
         val members = expression.split(" ").filter { it.isNotEmpty() }.toMutableList()
         simplifyOperators(members)
-        var sum = members.first().toInt()
+        var sum = getValue(members.first())
         for (index in 1..members.lastIndex) {
-            if (isNumber(members[index])) continue
+            if (!members[index].matches("""[-+]""".toRegex())) continue
 
             if (members[index] == "+") {
-                sum += members[index + 1].toInt()
+                sum += getValue(members[index + 1])
             } else {
-                sum -= members[index + 1].toInt()
+                sum -= getValue(members[index + 1])
             }
         }
         println(sum)
+    }
+
+    fun getValue(inputValue: String): Int {
+        return try {
+            inputValue.toInt()
+        } catch (e: NumberFormatException) {
+            var result = 0
+            if(globalVariables.containsKey(inputValue.trim())) {
+                result = globalVariables[inputValue.trim()]!!
+            } else {
+                printError("UnknownVariable")
+                throw Exception("Unknown variable")
+            }
+            result
+        }
     }
 
     fun simplifyOperators(members: MutableList<String>) {
@@ -55,13 +126,6 @@ class Calculator {
             if (members[index].matches("-+".toRegex())) {
                 members[index] = if (members[index].length % 2 == 0) "+" else "-"
             }
-        }
-    }
-
-    fun isNumber(s: String): Boolean {
-        return when(s.toIntOrNull()) {
-            null -> false
-            else -> true
         }
     }
 }
