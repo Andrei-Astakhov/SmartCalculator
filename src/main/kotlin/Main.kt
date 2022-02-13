9eebbb11346b202ead94f3417b854144cebd1f7b
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-02-13 18:30:43 +0300

    Stage 7/8: I’ve got the power

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,9 @@ package calculator
 
 class Calculator {
     val commandRegex = Regex("""/[a-z]+""")
-    val expressionRegex = Regex("""[-+]?(\d+|[a-zA-Z]+)(\s*[-+]*\s*-?(\d+|[a-zA-Z]+))*""")
-    val variableRegex = Regex("""\s*\w+(\s*=\s*\w+\s*)*""")
+    val expressionRegex = Regex("""-?(?:-?[0-9a-zA-Z()]+([-+]+|[*/]))+-?[0-9a-zA-Z()]+""")
+
+    val variableRegex = Regex("""\w+(=[-+]?\w+)*""")
     val globalVariables = mutableMapOf<String, Int>()
     val errors = mapOf(
         "InvalidAssignment" to "Invalid assignment",
@@ -16,12 +17,13 @@ class Calculator {
     fun run() {
         while (true) {
             val inputString = readLine()!!
+            val inputForMatch = inputString.replace("\\s*".toRegex(), "")
             try {
                 when {
-                    inputString.isBlank() -> continue
-                    inputString.matches(variableRegex)  -> processVariable(inputString)
-                    inputString.matches(expressionRegex) -> calculate(inputString)
-                    inputString.matches(commandRegex) -> {
+                    inputForMatch.isBlank() -> continue
+                    inputForMatch.matches(variableRegex)  -> processVariable(inputForMatch)
+                    inputForMatch.matches(expressionRegex) -> processExpression(inputForMatch)
+                    inputForMatch.matches(commandRegex) -> {
                         when (inputString) {
                             "/exit" -> break
                             "/help" -> showHelp()
@@ -38,17 +40,12 @@ class Calculator {
     }
 
     fun processVariable(expression: String) {
-        val variables = expression.split("""\s*=\s*""".toRegex())
+        val variables = expression.split("=")
         when (variables.size) {
             1 -> printVariable(variables.first())
             2 -> setVariable(variables)
             else -> printError("InvalidAssignment")
         }
-    }
-
-    fun printError(errorCode: String) {
-        if(errors.containsKey(errorCode)) println(errors.getValue(errorCode))
-        else println("UNKNOWN ERROR")
     }
 
     fun printVariable(variable: String) {
@@ -62,13 +59,13 @@ class Calculator {
     }
 
     fun setVariable(variables: List<String>) {
-        val leftValue = variables.first().trim()
-        val rightValue = variables.last().trim()
+        val leftValue = variables.first()
+        val rightValue = variables.last()
 
         when {
             !leftValue.matches("""[a-zA-Z]+""".toRegex()) -> printError("InvalidIdentifier")
-            rightValue.matches("""\d+""".toRegex()) -> globalVariables[leftValue] = rightValue.toInt()
-            rightValue.matches("""[a-zA-Z]+""".toRegex()) -> {
+            rightValue.matches("""[-]?\d+""".toRegex()) -> globalVariables[leftValue] = rightValue.toInt()
+            rightValue.matches("""[-]?[a-zA-Z]+""".toRegex()) -> {
                 if (globalVariables.containsKey(rightValue)) {
                     globalVariables[leftValue] = globalVariables[rightValue]!!.toInt()
                 } else {
@@ -79,42 +76,17 @@ class Calculator {
         }
     }
 
-    fun showHelp() {
-        println("The program calculates the sum of numbers.")
-        println("Your can enter several same operators following each other, e.g.: 9 +++ 10 -- 8.")
-        println("The even number of minuses gives a plus, and the odd number of minuses gives a minus!")
-        println("Look at it this way: 2 -- 2 equals 2 - (-2) equals 2 + 2.")
-    }
+    fun processExpression(expression: String) {
+        if (expression.count { it == '(' } != expression.count { it == ')' }) {
+            printError("InvalidExpression")
+            return
+        }
 
-    fun calculate(expression: String) {
-        val members = expression.split(" ").filter { it.isNotEmpty() }.toMutableList()
+        val members = expression.split("(?<=[\\d|a-zA-Z)(])(?=\\D)|(?<=\\D)(?=[\\d|a-zA-Z)(])".toRegex()).toMutableList()
+
         simplifyOperators(members)
-        var sum = getValue(members.first())
-        for (index in 1..members.lastIndex) {
-            if (!members[index].matches("""[-+]""".toRegex())) continue
-
-            if (members[index] == "+") {
-                sum += getValue(members[index + 1])
-            } else {
-                sum -= getValue(members[index + 1])
-            }
-        }
-        println(sum)
-    }
-
-    fun getValue(inputValue: String): Int {
-        return try {
-            inputValue.toInt()
-        } catch (e: NumberFormatException) {
-            var result = 0
-            if(globalVariables.containsKey(inputValue.trim())) {
-                result = globalVariables[inputValue.trim()]!!
-            } else {
-                printError("UnknownVariable")
-                throw Exception("Unknown variable")
-            }
-            result
-        }
+        val postfix = infixToPostfix(members)
+        calculate(postfix)
     }
 
     fun simplifyOperators(members: MutableList<String>) {
@@ -127,6 +99,123 @@ class Calculator {
                 members[index] = if (members[index].length % 2 == 0) "+" else "-"
             }
         }
+        members.replaceAll { it.replace("\\s*", "") }
+    }
+
+    fun getValue(inputValue: String): Int {
+        return try {
+            inputValue.toInt()
+        } catch (e: NumberFormatException) {
+            var result = 0
+            if(globalVariables.containsKey(inputValue)) {
+                result = globalVariables[inputValue]!!
+            } else {
+                printError("UnknownVariable")
+                throw Exception("Unknown variable")
+            }
+            result
+        }
+    }
+
+    fun infixToPostfix(infix: MutableList<String>): List<String> {
+        val postfixStack = mutableListOf<String>()
+        val tempStack = mutableListOf<String>()
+        for (s in infix) {
+            if (s.matches("[\\d|a-zA-Z]+".toRegex())) {
+                postfixStack += s
+            } else if (s == "(") {
+                tempStack.add(s)
+            } else if (s == ")") {
+                while (tempStack.isNotEmpty() && tempStack.last() != "(") {
+                    postfixStack += tempStack.last()
+                    tempStack.removeAt(tempStack.lastIndex)
+                }
+                if (tempStack.isNotEmpty() && tempStack.last() != "(") {
+                    printError("InvalidExpression")
+                    return emptyList()
+                } else {
+                    tempStack.removeAt(tempStack.lastIndex)
+                }
+            } else {
+                while (tempStack.isNotEmpty() && precedence(s) <= precedence(tempStack.last())) {
+                    postfixStack += tempStack.last()
+                    tempStack.removeAt(tempStack.lastIndex)
+                }
+                tempStack.add(s)
+            }
+        }
+
+        while (tempStack.isNotEmpty()) {
+            postfixStack += tempStack.last()
+            tempStack.removeAt(tempStack.lastIndex)
+        }
+
+        return postfixStack.toList()
+    }
+
+    fun precedence(operand: String): Int {
+        return when (operand) {
+            "+","-" -> 1
+            "*", "/" -> 2
+            else -> -1
+        }
+    }
+
+    fun calculate(postfixStack: List<String>) {
+        val calculationStack = mutableListOf<Int>()
+        for (el in postfixStack) {
+            when {
+                el.matches("[\\d|a-zA-Z]+".toRegex()) -> {
+                    calculationStack.add(getValue(el))
+                }
+                el == "*" -> {
+                    val op1 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    val op2 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    calculationStack.add(op2 * op1)
+                }
+                el == "/" -> {
+                    val op1 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    val op2 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    calculationStack.add(op2 / op1)
+                }
+                el == "+" -> {
+                    val op1 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    val op2 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    calculationStack.add(op2 + op1)
+                }
+                el == "-" -> {
+                    val op1 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    val op2 = calculationStack.last()
+                    calculationStack.removeAt(calculationStack.lastIndex)
+                    calculationStack.add(op2 - op1)
+                }
+            }
+        }
+
+        val result = calculationStack.last()
+        println(result)
+    }
+
+    fun showHelp() {
+        println("The program calculates arithmetic operation.")
+        println("Your can enter several same operators following each other, e.g.: 9 +++ 10 -- 8.")
+        println("The even number of minuses gives a plus, and the odd number of minuses gives a minus!")
+        println("Look at it this way: 2 -- 2 equals 2 - (-2) equals 2 + 2.")
+        println("You cannot specify several * or / operation as it is possible for + and -.")
+        println("It is possible to declare a variable: a=5 and use it in an expression.")
+        println("Valid name of a variable contains only letters.")
+    }
+
+    fun printError(errorCode: String) {
+        if(errors.containsKey(errorCode)) println(errors.getValue(errorCode))
+        else println("UNKNOWN ERROR")
     }
 }
 
